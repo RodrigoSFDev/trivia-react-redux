dbabbabb882b1addd001e76acd9464acf56f4e96
Amanda Rodrigues <rodriguesamanda49@gmail.com>
2023-05-22 21:10:26 -0300

    requisito 6: fetch da api e redirecionamento caso token seja inválido

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Game extends React.Component {
+  async componentDidMount() {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+      const data = await response.json();
+      const num = 3;
+      if (data.response_code === num) {
+        const { history } = this.props;
+        localStorage.removeItem('token');
+        history.push('/');
+      }
+      console.log(data);
+    } catch (err) {
+      console.log('Um erro foi capturado.', err);
+    }
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +30,15 @@ class Game extends React.Component {
   }
 }
 
-export default Game;
+Game.propTypes = {
+  // dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+export default connect(mapStateToProps)(Game);
