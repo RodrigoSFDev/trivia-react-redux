d850e1665adbaa815b7efaef15fc6fda787a9826
Amanda Rodrigues <rodriguesamanda49@gmail.com>
2023-05-24 17:00:13 -0300

    requisito 13: concluido

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import './Feedback.css';
 
 class Feedback extends Component {
   render() {
