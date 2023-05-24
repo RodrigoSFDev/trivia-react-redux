6c5914bb87c3ab468307c219daa183d6f9266072
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-05-24 15:43:11 -0300

    req 18

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
 import './App.css';
+import Ranking from './pages/Ranking';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,8 @@ class App extends React.Component {
           <Route exact path="/" component={ Login } />
           <Route path="/game" component={ Game } />
           <Route exact path="/settings" component={ Settings } />
+
+          <Route path="/ranking" component={ Ranking } />
         </Switch>
       </div>
     );
